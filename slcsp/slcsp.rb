d1c19df982a2b9e149d70afdb4dfca277b14c949
Alexandra Millatmal <alexandrajmh@gmail.com>
2017-10-16 10:45:29 -0500

    functionality appears to be working for zip 36750, which only has one rate area. much more refining to be done; driver to be built.

--- slcsp/slcsp.rb
+++ slcsp/slcsp.rb
@@ -1,11 +1,46 @@
+require 'pry'
+require 'csv'
+
 # returns true false for ambiguity of zip
 def check_zip(zip)
 end
 
-# finds silver plans in plans.csv for a given zip, returns plans as an array
-def find_silver_plans_for_zip(zip)
+# returns rate area for a given zip
+def find_rate_area(zip)
+  ### Refactor?
+  rate_areas = []
+  CSV.foreach("./zips.csv") do |row|
+    if row[0] == zip.to_s
+      rate_areas << row[4]
+    end
+  end
+  ###
+  rate_areas = rate_areas.uniq
+  if rate_areas.length == 1
+    return rate_areas[0]
+  else
+    return false
+  end
+end
+
+# finds silver plans in plans.csv for a rate area, returns plans as an array
+def find_silver_plans_for_area(rate_area)
+  silver_plans = []
+  CSV.foreach("./plans.csv") do |row|
+    if row[2] == "Silver" && row[4] == rate_area
+      silver_plans << row[3].to_f
+    end
+  end
+  return silver_plans.uniq.sort
 end
 
 # Returns string of slcp (second lowest cost plan) in the collection of plans for an area
 def find_slcp(silver_plans)
+  return silver_plans[1].to_s
 end
+
+
+rate_area = find_rate_area(36750)
+silver_plans = find_silver_plans_for_area(rate_area)
+print silver_plans
+print silver_plans.length
