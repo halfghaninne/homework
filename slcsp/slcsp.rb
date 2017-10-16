1fad0b8060293b34ff875908c869c5cc1ed27753
Alexandra Millatmal <alexandrajmh@gmail.com>
2017-10-16 09:46:09 -0500

    adds ruby file and bare bones of functions needed

---
+++ slcsp/slcsp.rb
@@ -0,0 +1,11 @@
+# returns true false for ambiguity of zip
+def check_zip(zip)
+end
+
+# finds silver plans in plans.csv for a given zip, returns plans as an array
+def find_silver_plans_for_zip(zip)
+end
+
+# Returns string of slcp (second lowest cost plan) in the collection of plans for an area
+def find_slcp(silver_plans)
+end
